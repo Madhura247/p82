805266bdc6484eaeb3b0fcd8acf47cdb037b4ff7
Madhura Yuvaraj Badhe <75791284+Madhura247@users.noreply.github.com>
2023-12-16 14:27:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerNavigation from "./navigation/DrawerNavigation";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNavigation/>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
